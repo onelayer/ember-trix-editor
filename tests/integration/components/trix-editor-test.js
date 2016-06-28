63b463adf6f288c2c729ac4e8471fcad3f8200cc
Matt McFarland <matmcfarland@gmail.com>
2016-06-28 11:43:30 -0500

    update dependencies and tests for new ember-cli release

--- tests/integration/components/trix-editor-test.js
+++ tests/integration/components/trix-editor-test.js
@@ -1,128 +1,134 @@
+/* jshint jquery: true */
+
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import TrixEditor from 'ember-trix-editor/components/trix-editor';
 
-moduleForComponent('trix-editor', 'Integration | Component | trix editor', {
+moduleForComponent("trix-editor", "Integration | Component | trix editor", {
   integration: true
 });
 
-function clearAndRender(template) {
-  $('#ember-testing').html('');
-  this.render(template);
+function renderAndFindElement(context, template) {
+  context.clearRender();
+  context.render(template);
+
+  return context.$("> div");
 }
 
-test('renders', function (assert) {
-  assert.expect(5);
+test("it renders", function (assert) {
+  let $trix = renderAndFindElement(this, hbs`{{trix-editor}}`);
 
-  clearAndRender.call(this, hbs``);
-  let $trixEditor = this.$().find('> div');
-  assert.strictEqual($trixEditor.length, 0);
-
-  clearAndRender.call(this, hbs`{{trix-editor}}`);
-  $trixEditor = this.$().find('> div');
-  assert.strictEqual($trixEditor.length, 1);
-  assert.strictEqual($trixEditor.find('> input').length, 1);
-  assert.strictEqual($trixEditor.find('> trix-toolbar').length, 1);
-  assert.strictEqual($trixEditor.find('> trix-editor').length, 1);
+  assert.expect(4);
+  assert.strictEqual($trix.length, 1);
+  assert.strictEqual($trix.find("> input").length, 1);
+  assert.strictEqual($trix.find("> trix-toolbar").length, 1);
+  assert.strictEqual($trix.find("> trix-editor").length, 1);
 });
 
-test('sets correct HTML attributes to inputId attribute', function (assert) {
+test("sets correct HTML attributes to inputId attribute", function (assert) {
+  const template = hbs`{{trix-editor inputId="attrs-id"}}`;
+  let $trix = renderAndFindElement(this, template);
+
   assert.expect(2);
-
-  clearAndRender.call(this, hbs`{{trix-editor inputId="attrs-id"}}`);
-  const inputIdAttribute = this.$().find('div > input').attr('id');
-  const customInputId = 'attrs-id';
-  assert.strictEqual(inputIdAttribute, customInputId);
-  const trixEditorInputAttribute = this.$().find('div > trix-editor').attr('input');
-  assert.strictEqual(trixEditorInputAttribute, customInputId);
+  assert.strictEqual($trix.find("> input").attr("id"), "attrs-id");
+  assert.strictEqual($trix.find("> trix-editor").attr("input"), "attrs-id");
 });
 
-test('is focused based on autofocus attribute', function(assert) {
+test("is focused based on autofocus attribute", function(assert) {
+  let template = hbs`{{trix-editor}}`;
+  let $trix = renderAndFindElement(this, template).find("trix-editor");
+
   assert.expect(7);
+  assert.notOk($trix.is(":focus"), "editor not focused by default");
 
-  clearAndRender.call(this, hbs`{{trix-editor}}`);
-  let $trixEditor = this.$().find('trix-editor');
-  assert.notOk($trixEditor.is(':focus'), 'editor not focused by default');
+  template = hbs`{{trix-editor autofocus=false}}`;
+  $trix = renderAndFindElement(this, template).find("trix-editor");
 
-  clearAndRender.call(this, hbs`{{trix-editor autofocus=false}}`);
-  $trixEditor = this.$().find('trix-editor');
-  assert.notOk($trixEditor.is(':focus'), 'editor not focused when autofocus attr is false');
+  assert.notOk($trix.is(":focus"), "editor not focused when autofocus attr is false");
 
-  clearAndRender.call(this, hbs`{{trix-editor autofocus=true}}`);
-  $trixEditor = this.$().find('trix-editor');
-  assert.ok($trixEditor.is(':focus'), 'editor focused when autofocus attr is true');
+  template = hbs`{{trix-editor autofocus=0}}`;
+  $trix = renderAndFindElement(this, template).find("trix-editor");
 
-  // Demonstrate that the component looks at the truthiness of the autofocus attribute.
+  assert.notOk($trix.is(":focus"), "editor not focused when autofocus attr is 0");
 
-  clearAndRender.call(this, hbs`{{trix-editor autofocus=0}}`);
-  $trixEditor = this.$().find('trix-editor');
-  assert.notOk($trixEditor.is(':focus'), 'editor not focused when autofocus attr is int 0');
+  template = hbs`{{trix-editor autofocus=""}}`;
+  $trix = renderAndFindElement(this, template).find("trix-editor");
 
-  clearAndRender.call(this, hbs`{{trix-editor autofocus=1}}`);
-  $trixEditor = this.$().find('trix-editor');
-  assert.ok($trixEditor.is(':focus'), 'editor focused when autofocus attr is int 1');
+  assert.notOk($trix.is(":focus"), "editor not focused when autofocus attr is a falsey string");
 
-  clearAndRender.call(this, hbs`{{trix-editor autofocus=""}}`);
-  $trixEditor = this.$().find('trix-editor');
-  assert.notOk($trixEditor.is(':focus'), 'editor not focused when autofocus attr is a falsey string');
+  template = hbs`{{trix-editor autofocus=true}}`;
+  $trix = renderAndFindElement(this, template).find("trix-editor");
 
-  clearAndRender.call(this, hbs`{{trix-editor autofocus="true"}}`);
-  $trixEditor = this.$().find('trix-editor');
-  assert.ok($trixEditor.is(':focus'), 'editor focused when autofocus attr is a truthy string');
+  assert.ok($trix.is(":focus"), "editor focused when autofocus attr is true");
+
+  template = hbs`{{trix-editor autofocus=1}}`;
+  $trix = renderAndFindElement(this, template).find("trix-editor");
+
+  assert.ok($trix.is(":focus"), "editor focused when autofocus attr is true");
+
+  template = hbs`{{trix-editor autofocus="true"}}`;
+  $trix = renderAndFindElement(this, template).find("trix-editor");
+
+  assert.ok($trix.is(":focus"), "editor focused when autofocus attr is a truthy string");
 });
 
-test('sets trix-editor\'s placeholder attribute to placeholder attr', function (assert) {
+test("sets trix-editor's placeholder attribute to placeholder attr", function (assert) {
+  let template = hbs`{{trix-editor}}`;
+  let $trix = renderAndFindElement(this, template).find("trix-editor");
+
   assert.expect(2);
+  assert.notOk($trix.attr("placeholder"), "placeholder empty if not set");
 
-  clearAndRender.call(this, hbs`{{trix-editor}}`);
-  let $trixEditor = this.$().find('trix-editor');
-  assert.notOk($trixEditor.attr('placeholder'), 'placeholder empty if not set');
+  template = hbs`{{trix-editor placeholder="test placeholder"}}`;
+  $trix = renderAndFindElement(this, template).find("trix-editor");
 
-  clearAndRender.call(this, hbs`{{trix-editor placeholder="test placeholder"}}`);
-  $trixEditor = this.$().find('trix-editor');
-  assert.strictEqual($trixEditor.attr('placeholder'),
-                     'test placeholder',
-                     'placeholder attribute determines placeholder text');
+  assert.strictEqual($trix.attr("placeholder"), "test placeholder",
+    "passing placeholder attribute should set trix-editor placeholder");
 });
 
-test('sets trix-editor\'s class attribute to editorClass attr', function (assert) {
+test("sets trix-editor's class attribute to editorClass attr", function (assert) {
+  let template = hbs`{{trix-editor}}`;
+  let $trix = renderAndFindElement(this, template).find("trix-editor");
+
   assert.expect(3);
+  assert.notOk($trix.attr("class"), "class is empty by default");
 
-  clearAndRender.call(this, hbs`{{trix-editor}}`);
-  let $trixEditor = this.$().find('trix-editor');
-  assert.notOk($trixEditor.attr('class'), 'class is empty by default');
+  template = hbs`{{trix-editor editorClass="one"}}`;
+  $trix = renderAndFindElement(this, template).find("trix-editor");
 
-  clearAndRender.call(this, hbs`{{trix-editor editorClass="one"}}`);
-  $trixEditor = this.$().find('trix-editor');
-  assert.ok($trixEditor.hasClass('one'), 'accepts class name from editorClass');
+  assert.ok($trix.hasClass("one"), "accepts class name from editorClass");
 
-  clearAndRender.call(this, hbs`{{trix-editor editorClass="one two"}}`);
-  $trixEditor = this.$().find('trix-editor');
-  assert.ok($trixEditor.hasClass('one two'), 'accepts multiple class names from editorClass');
+  template = hbs`{{trix-editor editorClass="one two"}}`;
+  $trix = renderAndFindElement(this, template).find("trix-editor");
+
+  assert.ok($trix.hasClass("one two"), "accepts multiple class names from editorClass");
 });
 
-test('sets trix-editor\'s event listeners on insert', function (assert) {
-  assert.expect(8);
+test("sets trix-editor's event listeners on insert", function (assert) {
+  let template = hbs`{{trix-editor}}`;
+  let trix = renderAndFindElement(this, template).find("trix-editor")[0];
+  let listeners = Object.keys(jQuery._data(trix, "events"));
 
-  clearAndRender.call(this, hbs`{{trix-editor}}`);
-  const trixEditorEl = this.$().find('trix-editor')[0];
-  const eventNames = Object.keys(jQuery._data(trixEditorEl, 'events'));
-  eventNames.forEach(eventName => {
-    assert.notEqual(eventNames.indexOf(eventName),
-                    -1,
-                    `${eventName} event is bound on insert`);
+  assert.expect(TrixEditor.EVENTS.length);
+
+  TrixEditor.EVENTS.forEach(eventName => {
+    assert.ok(listeners.indexOf(eventName) > -1,
+      `${eventName} should be handled by component`);
   });
 });
 
-test('removes trix-editor\'s event listeners on destruction', function (assert) {
-  assert.expect(2);
+test("removes trix-editor's event listeners on destruction", function (assert) {
+  let template = hbs`{{trix-editor}}`;
+  let trix = renderAndFindElement(this, template).find("trix-editor")[0];
 
-  clearAndRender.call(this, hbs`{{trix-editor}}`);
-  const $trixEditor = this.$().find('trix-editor');
-  const trixEditorEl = $trixEditor[0];
-  let eventsObject = jQuery._data(trixEditorEl, 'events');
-  assert.ok(eventsObject, 'events are bound initially on insert');
+  assert.expect(1);
+  this.clearRender();
 
-  $('#ember-testing').html('');
-  eventsObject = jQuery._data(trixEditorEl, 'events');
-  assert.notOk(eventsObject, 'events are removed on destruction');
+  let listeners = jQuery._data(trix, "events");
+
+  assert.notOk(listeners, "it removes all event listeners from DOM");
+});
+
+test("attachmentsDisabled should disable trix attachments", function (assert) {
+  assert.expect(1);
 });
